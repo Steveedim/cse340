198ec1975310c4b1c326cb15aa5a01590b124111
steveedim <elderedimstephen@gmail.com>
2026-05-08 17:24:16 +0100

    ejs

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-
 import {fileURLToPath} from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 const PORT = process.env.PORT || 3000;
@@ -30,8 +31,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
-  const title = 'Our Partner Organizations';
-  res.render('organizations', { title });
+const organizations = await getAllOrganizations();
+
+  const title = 'Organizations';
+  res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
@@ -44,7 +47,14 @@ app.get('/category', async (req, res) => {
     res.render('category', { title });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); // Exit with a failure code
+  }  
 });
